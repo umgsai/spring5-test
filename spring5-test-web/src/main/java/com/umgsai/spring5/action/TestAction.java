3377e33e1fea32cbd114ac1f7943e8d098c860b9
= <umgsai@126.com>
2017-04-04 20:00:47 +0800

    init

---
+++ spring5-test-web/src/main/java/com/umgsai/spring5/action/TestAction.java
@@ -0,0 +1,23 @@
+package com.umgsai.spring5.action;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Shang on 2017-4-4.
+ */
+@Controller
+public class TestAction {
+
+    @Getter
+    private String today;
+
+    @RequestMapping(value = "/test")
+    public String test (Model model) {
+        today = "2017-4-4 19:45:39";
+        model.addAttribute("today", today);
+        return "test";
+    }
+}
