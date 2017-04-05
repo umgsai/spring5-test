8cb2e1c3cd124ee45b81d04003af2330f5631cf4
shangyidong <shangyidong@meituan.com>
2017-04-05 19:19:20 +0800

    新建测试类

--- spring5-test-api/src/main/java/com/umgsai/spring5/test/api/TestService.java
+++ spring5-test-api/src/main/java/com/umgsai/spring5/test/api/TestService.java
@@ -3,5 +3,5 @@ package com.umgsai.spring5.test.api;
 /**
  * Created by shangyidong on 17/4/5.
  */
-public interface TestApi {
+public interface TestService {
 }
---
+++ spring5-test-biz/src/main/java/com/umgsai/spring5/test/biz/TestServiceImpl.java
@@ -0,0 +1,9 @@
+package com.umgsai.spring5.test.biz;
+
+import com.umgsai.spring5.test.api.TestService;
+
+/**
+ * Created by shangyidong on 17/4/5.
+ */
+public class TestServiceImpl implements TestService {
+}
